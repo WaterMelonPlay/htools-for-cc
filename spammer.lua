ce35ce2da3093592c65b924025045e8e6ca8f2c1
WateMelon <hacker.ilovesonya@gmail.com>
2025-01-16 08:29:15 +0000

    Улучшил код
    +производительность
    +скорость
    -большой размер
    -говнокод

--- spammer.lua
+++ spammer.lua
@@ -1,13 +1,1 @@
-rednet.open("back")
-
-count = read()
-count = tonumber(count)
-
-for i = 1, count do 
-    rednet.broadcast(" sss   ppp   aa  mmmm   mmmm")
-    rednet.broadcast("s      p  p a  a m m m  m m m")    
-    rednet.broadcast("ssss   pppp aaaa m m m  m m m")
-    rednet.broadcast("   s   p    a  a m m m  m m m")
-    rednet.broadcast("sss    p    a  a m m m  m m m")
-    rednet.broadcast("----------------------")
-end
+peripheral.find("modem",rednet.open)for i=1,tonumber(read())do rednet.broadcast(" sss   ppp   aa  mmmm   mmmm\ns      p  p a  a m m m  m m m\nssss   pppp aaaa m m m  m m m\n   s   p    a  a m m m  m m m\nsss    p    a  a m m m  m m m\n----------------------")end
